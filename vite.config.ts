cadeaca16513e181440b23e5a56bc24d2d975a05
okomeworld <rwmh0209@gmail.com>
2021-12-05 16:23:57 +0900

    フォーマッタの適用

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,16 @@
-import { defineConfig, loadEnv } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default ({ mode }) => {
-  const customEnv = loadEnv(mode, process.cwd())
+  const customEnv = loadEnv(mode, process.cwd());
 
   return defineConfig({
     plugins: [react()],
     define: {
-      'process.env.EMPATH_API_KEY': JSON.stringify(customEnv.VITE_EMPATH_API_KEY)
-    }
-  })
-}
+      "process.env.EMPATH_API_KEY": JSON.stringify(
+        customEnv.VITE_EMPATH_API_KEY
+      ),
+    },
+  });
+};
